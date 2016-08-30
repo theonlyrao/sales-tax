a40b427b2dc778ba08f3a4be609daa26785924fd
Ashwin Rao <theonlyrao@gmail.com>
2016-08-30 15:27:07 -0500

    Update index of items with all items
    Now all the items listed in the test cases are included in the item index.

--- src/main/java/ItemIndex.java
+++ src/main/java/ItemIndex.java
@@ -10,13 +10,15 @@ public class ItemIndex {
 
 	itemTypes.put("chocolate bar", "food");
 	itemTypes.put("book", "book");
+	itemTypes.put("headache pills", "medical product");
+	itemTypes.put("box of chocolates", "food");
+	itemTypes.put("box of imported chocolates", "food");
 
     }
 
     public String typeOf(String item) {
 	String type;
 	type = itemTypes.get(item);
-	System.out.println("item is " + item + " with type " + type);
 	return type;
     }
 }
