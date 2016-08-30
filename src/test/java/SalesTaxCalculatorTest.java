32c0b2e75d3360472776f1a18f7ee364aa411349
Ashwin Rao <theonlyrao@gmail.com>
2016-08-30 11:34:04 -0500

    Update test for new data type

--- src/test/java/SalesTaxCalculatorTest.java
+++ src/test/java/SalesTaxCalculatorTest.java
@@ -1,19 +1,19 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.List;
 
-eshellpublic class SalesTaxCalculatorTest {
+public class SalesTaxCalculatorTest {
     @Test public void testThreeDomesticCart() {
 	SalesTaxCalculator calculator = new SalesTaxCalculator();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-	String cartPath = "./cart/threeDomestic.txt";
+	String cartPath = "./src/test/java/carts/threeDomestic.txt";
 
-	String[] output;
+	List<String> output;
 	output = calculator.analyze(cartPath);
 
-	assertEquals("1 book: 12.49", output[0]);
-	assertEquals("1 music CD: 16.49", output[1]);
-	assertEquals("1 chocolate bar: 0.85", output[2]);
-	assertEquals("Sales Taxes: 1.50", output[3]);
-	assertEquals("Total: 29.83", output[4]);
+	assertEquals("1 book: 12.49", output.get(0));
+	assertEquals("1 music CD: 16.49", output.get(1));
+	assertEquals("1 chocolate bar: 0.85", output.get(2));
+	assertEquals("Sales Taxes: 1.50", output.get(3));
+	assertEquals("Total: 29.83", output.get(4));
     }
 }
