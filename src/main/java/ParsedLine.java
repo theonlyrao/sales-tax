fdb39a55a280a02d5b6e9d096084acf0f9bce947
Ashwin Rao <theonlyrao@gmail.com>
2016-08-31 17:47:41 -0500

    Identify item in string of line item

--- src/main/java/ParsedLine.java
+++ src/main/java/ParsedLine.java
@@ -9,11 +9,11 @@ public class ParsedLine {
     }
 
     public int quantity() {
-	String[] tmp = new String[5];
-	tmp = line.split("\\s+");
+	String[] a = new String[5];
+	a = line.split("\\s+", 2);
 	
 	int quantity;
-	quantity = Integer.parseInt(tmp[0]);
+	quantity = Integer.parseInt(a[0]);
 	return quantity;
     }
 
@@ -22,8 +22,17 @@ public class ParsedLine {
     }
 
     public String item() {
-	String item = "pizza";
-	return item;
+	String[] b1 = new String[5];
+	b1 = line.split("\\s+", 2);
+
+	String[] b2 = new String[5];
+	b2 = b1[1].split(" at");
+
+	if (b2[0].contains("imported")) {
+	    b2[0] = b2[0].replace("imported", "");
+	} 
+
+	return b2[0].trim();
     }
 
     public float subTotal() {
